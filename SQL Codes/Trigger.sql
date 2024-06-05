d12f48f3e5d2f72ea0180767602c7d27e25fd433
Marcelo Lino junior <145693935+Txelinho016@users.noreply.github.com>
2024-06-05 01:43:22 +0100

    Add files via upload

---
+++ SQL Codes/Trigger.sql
@@ -0,0 +1,95 @@
+CREATE TRIGGER TechHouse.Suppi
+ON TechHouse.Supplier
+AFTER UPDATE, INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT * 
+        FROM TechHouse.Technician 
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+    OR EXISTS (
+        SELECT * 
+        FROM TechHouse.Worker 
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+    BEGIN
+        RAISERROR('Conflito de Dados', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END
+GO
+
+CREATE TRIGGER TechHouse.Technicn
+ON TechHouse.Technician
+AFTER UPDATE, INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT * 
+        FROM TechHouse.Supplier 
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+    OR EXISTS (
+        SELECT * 
+        FROM TechHouse.Worker 
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+    BEGIN
+        RAISERROR('Conflito de Dados', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END
+GO
+
+CREATE TRIGGER TechHouse.Workr
+ON TechHouse.Worker
+AFTER UPDATE, INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT * 
+        FROM TechHouse.Supplier 
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+    OR EXISTS (
+        SELECT * 
+        FROM TechHouse.Technician 
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+    BEGIN
+        RAISERROR('Conflito de Dados', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END
+GO
+
+-- Triggers EmployeeTypes
+CREATE TRIGGER TechHouse.EmployeeType_Disjoint
+ON TechHouse.EmployeeType
+AFTER UPDATE, INSERT
+AS
+BEGIN
+    IF NOT EXISTS(
+        SELECT * 
+        FROM TechHouse.Supplier
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+    OR NOT EXISTS(
+        SELECT * 
+        FROM TechHouse.Technician
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+	OR NOT EXISTS(
+        SELECT * 
+        FROM TechHouse.Worker
+        WHERE EmployeeID IN (SELECT EmployeeID FROM inserted)
+    )
+    BEGIN
+        RAISERROR('Conflito de dados.', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END
+GO
+
+
